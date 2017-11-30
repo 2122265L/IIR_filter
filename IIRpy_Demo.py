f599b9bf923a836a87f2db5b9e46bee230cbcfd8
2122265L <32393049+2122265L@users.noreply.github.com>
2017-11-30 21:22:21 +0000

    Version 2 IIRpy_demo
    This version includes stop band (ws) and start band (wp) frequency specification as well as gpass and gstop which are the minimum attenuation factors in the pass and stop bands.

--- IIRpy_Demo.py
+++ IIRpy_Demo.py
@@ -18,22 +18,23 @@ y = np.zeros(fs)
 
 
 def Filt(Ftype):
-    N = 2
-    fc = [290/fs,310/fs, 390/fs, 420/fs]
-
-    f  = IIRpy.IIR(N, fc, Ftype)
-
+    N = 10
+    fc = [290/fs, 310/fs, 390/fs, 420/fs, 460/fs]
+    
+    fws =[270/fs, 330/fs, 370/fs, 440/fs, 440/fs]
+    
+    f  = IIRpy.IIR(fc, Ftype, ord = N, ws = fws)
+    
     for i in range(len(signal)):
         y[i] = f.filter(signal[i])
-        
+    
     pl.figure()
     pl.title("signal frequency for a %s, order %d" % (Ftype, N))
     pl.ylabel('f/Hz')
     pl.xlim(0,fs//2)
     pl.plot(np.arange(0,1000),abs(np.fft.fft(y)))
-
+    
 Ftype = ['lowpass', 'highpass', 'bandpass', 'bandstop']
 
 for i in range(4):
     Filt(Ftype[i])
-    
